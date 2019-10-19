0493aac0c0e159bd7e44e355e90ecb428c7d833a
Alexey <alexey.rykhalskiy@gmail.com>
2019-10-19 11:48:56 +0400

    lesson 2

--- src/main/java/lesson02/warmup/RandomMirror.java
+++ src/main/java/lesson02/warmup/RandomMirror.java
@@ -6,15 +6,22 @@ import java.util.Arrays;
 public class RandomMirror {
 
   public static int[] filter_negatives(int[] origin) {
+    int[] result = new int[origin.length];
+    int pos = 0;
 
+    for (int item: origin) {
+      if (item < 0) result[pos++] = item;
+    }
+
+    return pos == origin.length ? result : Arrays.copyOf(result, pos);
   }
 
   public static int[] filter_odd(int[] origin) {
-
+    throw new IllegalArgumentException("not implemented yet");
   }
 
   public static int[] filter_even(int[] origin) {
-
+    throw new IllegalArgumentException("not implemented yet");
   }
 
   public static void main(String[] args) {
