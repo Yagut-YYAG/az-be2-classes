611739ac5b54ea32d8524527d94d8e9013c51f04
Alexey <alexey.rykhalskiy@gmail.com>
2019-10-22 08:53:51 +0400

    lesson 4 ideas

---
+++ src/main/java/lesson04/warmup/Task1.java
@@ -0,0 +1,14 @@
+package lesson04.warmup;
+
+public class Task1 {
+
+  public static String gen_random(int length) {
+    ...
+    return ..
+  }
+
+  public static void main(String[] args) {
+    String s = gen_random(10);
+    System.out.println(s);
+  }
+}
